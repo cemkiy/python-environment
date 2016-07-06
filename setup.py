3d3916a41a43df36e47698be3ac1759b06054b1a
cemkiy <cemekiy@gmail.com>
2016-07-06 10:49:41 +0300

    add env methods

---
+++ setup.py
@@ -0,0 +1,17 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='python-environment',
+    version='0.0.1',
+    packages=find_packages(),
+    url='https://github.com/cemkiy/python-environment',
+    license='GNU GENERAL PUBLIC LICENSE',
+    author='Cem Kıy',
+    author_email='cemekiy@gmail.com',
+    description='python envronment management',
+    install_requires=[],
+    classifiers=(
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 3.5.1',
+    ),
+)
